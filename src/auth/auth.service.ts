2c2da9daf64584683bc7f8339b4f1e9826a2dfa8
Vladimir <pro.barsukov@gmail.com>
2022-04-21 11:32:17 +0200

    Init schema with Prisma

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+   signin() {
+    return { msg: 'I have signin'}
+     
+   }
+
+   signup() {
+    return { msg: 'I have sign up'}
+   }
+}
